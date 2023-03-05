45560341a2607493ecaec3718e36c1e19fe56be5
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-03-05 14:39:30 +0530

    account page added

--- src/components/profile/ProfileForm.js
+++ src/components/profile/ProfileForm.js
@@ -1,11 +1,118 @@
-import React from 'react';
+import * as Yup from 'yup';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { Box, Grid, Card, Stack, Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
 
-const ProfileForm = () => {
-  return (
-    <>
-      <h1>Profile Form</h1>
-    </>
+import { FormProvider, RHFTextField, RHFUploadAvatar } from '../hook-form';
+import { fData } from '../../utils/formatNumber';
+
+function ProfileForm() {
+  const UpdateUserSchema = Yup.object().shape({
+    displayName: Yup.string().required('Name is required'),
+    email: Yup.string().email(),
+    phoneNumber: Yup.string().required('required').min(10, 'to short').max(10, 'to long'),
+    address: Yup.string().required('Address is required'),
+    state: Yup.string().required('State is required'),
+    city: Yup.string().required('City is required'),
+    zipCode: Yup.number()
+      .min(100000, 'Must be exactly 6 characters')
+      .max(999999, 'Must be exactly 6 characters')
+      .required('ZipCode is required'),
+    about: Yup.string().required('About is required'),
+  });
+
+  const methods = useForm({
+    resolver: yupResolver(UpdateUserSchema),
+  });
+
+  const {
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = async (formData) => {
+    console.log('formdata', formData);
+  };
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      if (file) {
+        setValue(
+          'photoURL',
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        );
+      }
+    },
+    [setValue]
   );
-};
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ py: 10, px: 3, textAlign: 'center' }}>
+            <RHFUploadAvatar
+              name="photoURL"
+              accept="image/*"
+              maxSize={3145728}
+              onDrop={handleDrop}
+              helperText={
+                <Typography
+                  variant="caption"
+                  sx={{
+                    mt: 2,
+                    mx: 'auto',
+                    display: 'block',
+                    textAlign: 'center',
+                    color: 'text.secondary',
+                  }}
+                >
+                  Allowed *.jpeg, *.jpg, *.png, *.gif
+                  <br /> max size of {fData(3145728)}
+                </Typography>
+              }
+            />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} md={8}>
+          <Card sx={{ p: 3 }}>
+            <Box
+              sx={{
+                display: 'grid',
+                rowGap: 3,
+                columnGap: 2,
+                gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
+              }}
+            >
+              <RHFTextField name="displayName" label="Name" />
+              <RHFTextField disabled name="email" label="Email Address" />
+              <RHFTextField name="phoneNumber" label="Phone Number" />
+              <RHFTextField name="address" label="Address" />
+
+              <RHFTextField name="state" label="State/Region" />
+              <RHFTextField name="city" label="City" />
+              <RHFTextField name="zipCode" label="Zip/Code" />
+            </Box>
+            <Stack spacing={3} alignItems="flex-end" sx={{ mt: 3 }}>
+              <RHFTextField name="about" multiline rows={4} label="About" />
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                Update
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
+    </FormProvider>
+  );
+}
 
 export default ProfileForm;
--- src/pages/AccountGeneral.js
+++ src/pages/AccountGeneral.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Container, Button, Grid, Box, Stack, Typography } from '@mui/material';
 import Iconify from '../components/iconify';
+import ProfileForm from '../components/profile/ProfileForm';
 
 function AccountGeneral() {
   return (
@@ -9,15 +10,16 @@ function AccountGeneral() {
       <Helmet>
         <title> Dashboard: Account </title>
       </Helmet>
-      <Container>
+      <Container maxWidth="lg">
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Account
           </Typography>
         </Stack>
-        <Grid container sx={{ marginTop: '5rem' }} spacing={3}>
-          ACCOUNT PAGE
-        </Grid>
+        <Box sx={{ mb: 5 }} />
+        <Box>
+          <ProfileForm />
+        </Box>
       </Container>
     </>
   );
